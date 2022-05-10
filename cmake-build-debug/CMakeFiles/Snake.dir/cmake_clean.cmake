c71b56e4d3a358082ae65690a5f090b93dd2a86e
Yaroslav <Yaroslav@DOKA>
2022-05-10 15:53:34 +0400

    initial commit

---
+++ cmake-build-debug/CMakeFiles/Snake.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Snake.dir/main.cpp.obj"
+  "Snake.exe"
+  "Snake.exe.manifest"
+  "Snake.lib"
+  "Snake.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Snake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
